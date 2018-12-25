b823a240a0d91393c72dff84870d8be863c2a9c8
Abhishek Pokhrel <avipokh@gmail.com>
2018-12-25 16:53:34 -0500

    Added new page after login.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {NavController} from "@ionic/angular";
+import {Tab2Page} from "../tab2/tab2.page";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  constructor(public navCtrl: NavController) { }
+
+  onSignIn() {
+    console.log('button clicked');
+    this.navCtrl.navigateForward('/tabs/(tab2:tab2)');
+  }
+}
